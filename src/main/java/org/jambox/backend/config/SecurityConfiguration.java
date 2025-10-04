770b6a4588834eb860595903cc68253c0dc19555
Andre Bogner <andre@andrebogner.de>
2025-10-04 11:43:43 +0200

    updated `SecurityConfiguration`: changed default access policy to require authentication for all requests

--- src/main/java/org/jambox/backend/config/SecurityConfiguration.java
+++ src/main/java/org/jambox/backend/config/SecurityConfiguration.java
@@ -117,7 +117,7 @@ public class SecurityConfiguration {
                                         "/login/oauth2/**",     // OAuth2-Login-Endpunkte
                                         "/logout"
                                         ).permitAll()
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                         )
                 .logout(logout -> logout
                         .logoutUrl("/logout")                    // URL für Logout-Anfragen
